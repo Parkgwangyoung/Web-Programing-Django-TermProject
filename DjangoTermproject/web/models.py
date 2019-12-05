b7133b8ec0b3504ea21dc408ba79335d6f9d50ce
마블루 <57721151+Parkgwangyoung@users.noreply.github.com>
2019-12-06 01:34:52 +0900

    게시판 권한 수정

--- DjangoTermproject/web/models.py
+++ DjangoTermproject/web/models.py
@@ -11,6 +11,8 @@ class BoardTable(models.Model):
 class Board(models.Model):
     board_type = models.ForeignKey(BoardTable,on_delete=models.CASCADE,verbose_name="하위 게시판이름")
     board_name = models.CharField(max_length=20,verbose_name="게시판 이름")
+    supervisor = models.CharField(max_length=10, verbose_name="지도 교수")
+    grade = models.CharField(max_length=10,verbose_name="학년")
     
     def __str__(self):
         return self.board_name
@@ -25,6 +27,7 @@ class Post(models.Model):
     writer = models.CharField(max_length=10,verbose_name="작성자")
     writer_email = models.EmailField(max_length=30,verbose_name="작성자 아이디")
     date = models.DateField(auto_now=True,verbose_name="작성일")
+    
     like_number  = models.PositiveIntegerField(default=0,verbose_name="추천수")
 
     class Meta:
