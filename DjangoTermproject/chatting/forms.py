22e902b4dbebd5058d0734c7ea3eba660d35da4f
마블루 <57721151+Parkgwangyoung@users.noreply.github.com>
2019-12-12 01:18:13 +0900

    채팅 구현(수정필요)

---
+++ DjangoTermproject/chatting/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.forms import ModelForm
+from chatting.models import Chat
+
+
+
+class chatform(forms.ModelForm):
+    class Meta:
+        model = Chat
+        fields = ["text","writer","writer_email","date"]
+        exclude = {'writer','writer_email','date'}
+        widgets = {
+            'text':forms.TextInput(attrs={'size':'30','style':'height:35px;','placeholder':'글 입력'})
+        }
+        
--- DjangoTermproject/chatting/views.py
+++ DjangoTermproject/chatting/views.py
@@ -1,9 +1,40 @@
 from django.shortcuts import render
 from django.views import View
+from chatting.models import Chat
+from chatting.forms import chatform 
+from django.http import HttpResponse,HttpResponseRedirect
+from login.models import Student,Professor
+from django.urls import reverse
+import itertools
 # Create your views here.
 
 
 
 class chatView(View):
     def get(self,request,*args,**kwargs):
-        return render(request,'chatting/room.html')
+        chatting = Chat.objects.all().order_by('-date')[:5]    
+        writers = []
+        texts=[]
+        dates=[]
+        for i in chatting:
+            writers.append(i.writer)
+            texts.append(i.text)
+            dates.append(i.date)
+        writers.reverse()      
+        texts.reverse()
+        dates.reverse()
+        chat = zip(texts,writers,dates)        
+        form = chatform()
+        return render(request,'chatting/room.html',{'chat':chat,'form':form})
+
+    def post(self,request,*args,**kwargs):
+        student = Student.objects.get(id = request.session['logined_student_id'])
+        form = chatform(request.POST)
+        if form.is_valid():
+            chat = form.save(commit=False)
+            chat.writer = student.name
+            chat.writer_email = student.email
+            chat.save()
+            return HttpResponseRedirect(reverse('chatting:chat'))
+
+
